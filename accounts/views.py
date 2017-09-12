f49f7fd2a5471cdec3d07ac51bdbcecb533ce549
Jack11709 <jackmay89@live.com>
2017-09-12 16:26:52 +0100

    pushing reddit clone

---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_conf']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username already taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                login(request, user)
+                return render(request, 'accounts/login.html')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords did not match'})
+    else:
+        return render(request, 'accounts/signup.html')
+
+def loginview(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'Username or Password incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
+
+def logoutview(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
